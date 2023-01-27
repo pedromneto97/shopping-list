a8455e90361d3a832492bcb4edc055acc68288ed
Pedro Neto <pedromneto97@gmail.com>
2023-01-27 19:54:01 -0300

    feat: add shopping list activity

---
+++ app/src/main/java/br/eng/pedro_mendes/shopping_list/ui/add_shoping_list_item/AddShoppingListItemActivity.kt
@@ -0,0 +1,15 @@
+package br.eng.pedro_mendes.shopping_list.ui.add_shoping_list_item
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.eng.pedro_mendes.shopping_list.databinding.ActivityAddShoppingListItemBinding
+
+class AddShoppingListItemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddShoppingListItemBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddShoppingListItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
