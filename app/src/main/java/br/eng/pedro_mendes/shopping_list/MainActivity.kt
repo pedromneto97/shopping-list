78fe50b93bb04d69ab648bb2f3fc9db2ffb2beac
Pedro Neto <pedromneto97@gmail.com>
2023-01-27 19:27:40 -0300

    Initial commit

---
+++ app/src/main/java/br/eng/pedro_mendes/shopping_list/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.eng.pedro_mendes.shopping_list
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
