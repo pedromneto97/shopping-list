8ef415a9e5bde001f471dd7b15008e4adcef5d45
Pedro Neto <pedromneto97@gmail.com>
2023-01-27 20:59:56 -0300

    feat: improve shopping list item

--- app/src/main/java/br/eng/pedro_mendes/shopping_list/data/ShoppingListItem.kt
+++ app/src/main/java/br/eng/pedro_mendes/shopping_list/data/ShoppingListItem.kt
@@ -1,8 +1,12 @@
 package br.eng.pedro_mendes.shopping_list.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ShoppingListItem(
     var name: String,
-    var check: Boolean,
-    var quantity: Double,
+    var check: Boolean = false,
+    var quantity: Int,
     var measure: Measure,
-)
+) : Parcelable
