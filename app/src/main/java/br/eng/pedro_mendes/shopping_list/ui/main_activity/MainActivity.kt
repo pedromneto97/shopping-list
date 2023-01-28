41293167707c4a5e22c51e144ac349f2bf874e10
Pedro Neto <pedromneto97@gmail.com>
2023-01-27 21:01:03 -0300

    feat: receive value from other activity

--- app/src/main/java/br/eng/pedro_mendes/shopping_list/ui/main_activity/MainActivity.kt
+++ app/src/main/java/br/eng/pedro_mendes/shopping_list/ui/main_activity/MainActivity.kt
@@ -1,18 +1,32 @@
 package br.eng.pedro_mendes.shopping_list.ui.main_activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import br.eng.pedro_mendes.shopping_list.data.ShoppingListItem
 import br.eng.pedro_mendes.shopping_list.databinding.ActivityMainBinding
 import br.eng.pedro_mendes.shopping_list.ui.add_shopping_list_item.AddShoppingListItemActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val activityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK) {
-                //TODO Handle result
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                activityResult.data?.let {
+                    if (it.hasExtra(RESULT_TAG)) {
+                        val shoppingListItem =
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                it.getParcelableExtra(RESULT_TAG, ShoppingListItem::class.java)
+                            } else {
+                                it.getParcelableExtra<ShoppingListItem>(RESULT_TAG)
+                            }
+                        Log.i("RESULT", shoppingListItem.toString())
+                    }
+                }
+
             }
         }
 
@@ -34,4 +48,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+        const val RESULT_TAG = "ADD_SHOPPING_LIST_ITEM"
+    }
 }
