e494640e7f5860d9f6aadbf60fce4c445cd8b268
Pedro Neto <pedromneto97@gmail.com>
2023-01-28 23:30:52 -0300

    feat: add main view model

---
+++ app/src/main/java/br/eng/pedro_mendes/shopping_list/ui/main_activity/MainViewModel.kt
@@ -0,0 +1,35 @@
+package br.eng.pedro_mendes.shopping_list.ui.main_activity
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.eng.pedro_mendes.shopping_list.data.ShoppingListItem
+import br.eng.pedro_mendes.shopping_list.data.repository.ShoppingListRepository
+
+class MainViewModel : ViewModel() {
+    private val shoppingListRepository = ShoppingListRepository()
+    private val innerShoppingList = MutableLiveData<List<ShoppingListItem>>()
+
+    val shoppingList: LiveData<List<ShoppingListItem>> = innerShoppingList
+
+    fun initialize() = updateShoppingList()
+
+    private fun updateShoppingList() {
+        innerShoppingList.value = shoppingListRepository.get()
+    }
+
+    fun setCheck(int: Int, isChecked: Boolean) {
+        shoppingListRepository.setChecked(int, isChecked)
+        updateShoppingList()
+    }
+
+    fun clear() {
+        shoppingListRepository.clear()
+        updateShoppingList()
+    }
+
+    fun add(shoppingListItem: ShoppingListItem) {
+        shoppingListRepository.add(shoppingListItem)
+        updateShoppingList()
+    }
+}
